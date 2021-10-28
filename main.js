c17f871eb1e0599236247cff05f084284827923a
Legend <me@liux.pro>
2021-10-28 19:57:40 +0800

    camera

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ function main() {
             message: 'Hello Vue!',
             cameras: [],
             camera: null,
-            stream: null
+            stream: null,
+            flag: true,
+            a: 0
         }
         , methods: {
             cam() {
@@ -27,7 +29,6 @@ function main() {
                         audio: false
                     }).then(function (stream1) {
                         //video.src = window.URL.createObjectURL(stream);
-
                         video.srcObject = stream1;
                         app.stream = stream1
                         video.play();
@@ -39,8 +40,12 @@ function main() {
                 }
             }
         }, watch: {
-            stream(){
-                getCameras(this.cameras)
+            stream(newStream) {
+                if (this.flag) {
+                    this.flag = false
+                    getCameras(this.cameras)
+                    this.camera = newStream.getVideoTracks()[0].getSettings().deviceId
+                }
             }
         }, computed: {
             cameraWidth() {
@@ -69,8 +74,6 @@ function main() {
     app.cam()
 
 
-
-
 }
 
 function onOpenCvReady() {
@@ -90,17 +93,48 @@ function cvMain(video) {
 
     //get a  square
     let a = Math.min(app.cameraWidth, app.cameraHeight)
+    this.a = a
 
 
     let square = new cv.Rect(0, 0, a, a);
 
+    let p1  = new cv.Point(a*0.125, a*0.125);
+    let p2  = new cv.Point(a*0.375, a*0.125);
+    let p3  = new cv.Point(a*0.625, a*0.125);
+    let p4  = new cv.Point(a*0.875, a*0.125);
+    let p5  = new cv.Point(a*0.875, a*0.375);
+    let p6  = new cv.Point(a*0.875, a*0.625);
+    let p7  = new cv.Point(a*0.875, a*0.875);
+    let p8  = new cv.Point(a*0.625, a*0.875);
+    let p9  = new cv.Point(a*0.375, a*0.875);
+    let p10  = new cv.Point(a*0.125, a*0.875);
+    let p11  = new cv.Point(a*0.125, a*0.625);
+    let p12  = new cv.Point(a*0.125, a*0.375);
+
+
+
+
+
+    let gray = [0, 255, 0, 255]
 
     function processVideo() {
         try {
             cap.read(src);
-            cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-            cv.imshow("canvasOutput", dst);
-            cv.imshow("roi", dst.roi(square))
+            let roi = src.roi(square);
+            cv.line(roi, p1, p4, [0, 255, 0, 100], 5)
+            cv.line(roi, p4, p7, [0, 255, 0, 100], 5)
+            cv.line(roi, p7, p10, [0, 255, 0, 100], 5)
+            cv.line(roi, p10, p1, [0, 255, 0, 100], 5)
+            cv.line(roi, p12, p5, [0, 255, 0, 100], 5)
+            cv.line(roi, p11, p6, [0, 255, 0, 100], 5)
+            cv.line(roi, p2, p9, [0, 255, 0, 100], 5)
+            cv.line(roi, p3, p8, [0, 255, 0, 100], 5)
+
+            for (let i = 0; i < 100; i++) {
+                roi.ucharPtr(i,i)[0]=255
+            }
+
+            cv.imshow("roi", roi)
 
             requestAnimationFrame(processVideo)
         } catch (err) {
